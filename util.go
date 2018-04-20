32c4d8397c96323dbf7588b7562374ccf84be17a
Samuel Palmeira <samuelpalmeira@outlook.com>
2018-04-19 22:20:27 -0300

    Adds PrintHelp function

--- util.go
+++ util.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+// PrintInfo print out subtitle manager logo and some infos.
 func PrintInfo() {
 	messageInfo := []byte("+--------------------------------------------------------------------------+\n|█▀▀ █░░█ █▀▀▄ ▀▀█▀▀ ░▀░ ▀▀█▀▀ █░░ █▀▀ █▀▀   █▀▀ ░▀░ █░░ ▀▀█▀▀ █▀▀ █▀▀█ █▀▀|\n|▀▀█ █░░█ █▀▀▄ ░░█░░ ▀█▀ ░░█░░ █░░ █▀▀ ▀▀█   █▀▀ ▀█▀ █░░ ░░█░░ █▀▀ █▄▄▀ ▀▀█|\n|▀▀▀ ░▀▀▀ ▀▀▀░ ░░▀░░ ▀▀▀ ░░▀░░ ▀▀▀ ▀▀▀ ▀▀▀   ▀░░ ▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀ ▀░▀▀ ▀▀▀|\n+--------------------------------------------------------------------------+\n")
 
 	fmt.Printf("%s", messageInfo)
+}
+
+// PrintHelp print out basic instructions of how to use subtitle manager.
+func PrintHelp() {
+	fmt.Printf(`
+Flags: -p="Define o caminho a ser executado a busca" -e="Define a extensão do arquivo" -v="Define a versão do arquivo"
+=== HOW TO USE ===
+* Execute o programa com a flag [-p] para definir o local de execução. eg: "./subs/" (Obrigatorio)
+* Você pode definir a extensão do arquivo a ser exportado. eg: ".sub" (Opcional)
+* Você pode definir a versão do arquivo. eg: "720p-WEB". (Opcional)
+* Você pode definiar a pasta para onde os arquivos serão movidos. eg: "./meus-subs/" (Opcional)
+	
+Caso ocorra algum problema, ou tenha alguma sugestão por favor me envie um email: samuelpalmeira@outlook.com
+	`)
 }
