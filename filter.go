b2d174a129e6f4d1ade8188a75b9312034c02e0e
Samuel Palmeira <samuelpalmeira@outlook.com>
2018-04-21 17:07:54 -0300

    Adds move func and fix copy func

--- filter.go
+++ filter.go
@@ -1,11 +1,9 @@
-// Subtitles Manager - Suman ??
-// TODO:
-// 1) Implement a new function that only moves files instead delete them.
-// 2) Implement a new function that organize files by folders that have the name of the serie or movie
+// TODO: Implement a new function that organize files by folders that have the name of the serie or movie
 
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -17,6 +15,7 @@ import (
 	"time"
 )
 
+// File used to save files info
 type File struct {
 	Path   string
 	Name   string
@@ -25,15 +24,32 @@ type File struct {
 	Folder string
 }
 
-var sub File
+// Sub used to save files content
+type Sub struct {
+	Name string
+	Path string
+	Body *bytes.Buffer
+}
+
+// Log used to save errors log
+type Log struct {
+	Func string
+	Err  error
+}
+
 var options = make([]string, 4)
 var flags = make([]string, 3)
 var wg sync.WaitGroup
+var mtx sync.Mutex
+var dst string
+var errFound bool
+var logs []Log
 
 const (
 	path    = 0
 	ext     = 1
 	version = 2
+	move    = 3
 )
 
 func init() {
@@ -41,9 +57,10 @@ func init() {
 	e := flag.String("e", ".srt", "Set the extension of the file.")
 	v := flag.String("v", "", "Set the version of the subtitle.")
 	h := flag.Bool("h", false, "Returns basic instructions to use Subtitle Manager.")
+	m := flag.String("m", "", "Only move files to this selected directory.")
 	flag.Parse()
-	flags = []string{*p, *e, *v}
-	if *h == true {
+	flags = []string{*p, *e, *v, *m}
+	if *h {
 		PrintInfo()
 		PrintHelp()
 		os.Exit(1)
@@ -51,43 +68,42 @@ func init() {
 }
 
 func main() {
-	var dst string
 	start := time.Now()
+
 	PrintInfo()
 	if flags[path] == "" {
-		fmt.Fprintf(os.Stdout, "Task stopped b/c path isn't declared.")
+		fmt.Fprintf(os.Stdout, "flags: you must define flag -p.")
 		os.Exit(1)
 	}
+
 	sf, err := getall()
 	if err != nil {
-		fmt.Fprintf(os.Stdout, "Task stopped because: %v\n", err)
+		fmt.Fprintf(os.Stdout, "system: unable to complete task because: %v\n", err)
 		os.Exit(1)
 	}
-	// Copy files to temp folder
-	dst = filepath.Join(os.TempDir(), "/subfilter")
-	copyall(sf, dst)
 
-	// Delete all files from source folder
-	deleteall(flags[path], flags[ext])
+	subs := copyall(sf)
 
-	// Copy files to source folder
-	copyall(sf, flags[path])
-
-	// Delete all files from temp folder
-	deleteall(dst, flags[ext])
+	// Main function
+	core(subs)
 
 	end := time.Since(start).Seconds()
+	if errFound {
+		fmt.Fprintf(os.Stdout, "*** TASK COMPLETED in %.2fs with some errors. Open log file to see all program execution errors ***", end)
+		if err := CreateLogFile(logs); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		return
+	}
 	fmt.Fprintf(os.Stdout, "*** TASK COMPLETED in %.2fs without any errors. ***", end)
 }
 
 func getall() (files []File, err error) {
-	var named = false
-	if flags[version] != "" {
-		named = !named
-	}
+
 	err = filepath.Walk(flags[path], func(path string, info os.FileInfo, err error) error {
 		if filepath.Ext(path) == flags[ext] {
-			if named {
+			if flags[version] != "" {
 				if match, _ := regexp.MatchString("([a-zA-Z0-9]+)."+flags[version], path); !match {
 					return nil
 				}
@@ -111,51 +127,49 @@ func getall() (files []File, err error) {
 	return files, err
 }
 
-func copyall(files []File, dst string) {
-	// Create or check if the output dst exists
-	if _, err := os.Stat(dst); os.IsNotExist(err) {
-		os.Mkdir(dst, 0666)
-		fmt.Fprintf(os.Stdout, "Temp dir: %s\n", dst)
-	}
-	fmt.Fprintf(os.Stdout, "Starting to copy all files...\n")
+func copyall(files []File) (subs []Sub) {
+	fmt.Fprintf(os.Stdout, "copyAllFiles: starting to copy all files...\n")
 
 	wg.Add(len(files))
 	for _, file := range files {
-		go copy(file, dst)
+		go copy(file, &subs)
 	}
 	wg.Wait()
-	fmt.Fprintf(os.Stdout, "All files have been copied!\n")
+	fmt.Fprintf(os.Stdout, "copyAllFiles: all files have been copied!\n")
+	return subs
 }
 
-func copy(file File, dst string) {
+func copy(file File, subs *[]Sub) {
 	defer wg.Done()
 	start := time.Now()
-	dsti := filepath.Join(dst, "/", file.Name)
-	// First verify if file already exists inside dst folder
-	dstfile, err := os.OpenFile(dsti, syscall.O_CREAT|syscall.O_EXCL, 0666)
+	buf := &bytes.Buffer{}
+
+	src, err := os.Open(file.Path)
 	if err != nil {
-		fmt.Fprintf(os.Stdout, "While checking an error occurs: %v\n", err)
+		fmt.Fprintf(os.Stdout, "copyFile: could not open file: %s.\n Error: %s\n", file.Path, err)
+		errFound = true
+		log := Log{
+			Func: "copy",
+			Err:  err,
+		}
+		logs = append(logs, log)
 		return
 	}
-	defer dstfile.Close()
+	defer src.Close()
+	io.Copy(buf, src)
 
-	srcfile, err := os.Open(file.Path)
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "While opening an error occurs: %v\n", err)
-		return
-	}
-	defer srcfile.Close()
-
-	if _, err := io.Copy(dstfile, srcfile); err != nil {
-		fmt.Fprintf(os.Stdout, "Task stopped, was not possible to copy the files.\n File: %s\n", file.Path)
-		os.Exit(2)
+	// Save subtitle body
+	sub := Sub{
+		Name: file.Name,
+		Path: file.Path,
+		Body: buf,
 	}
 
-	// Set path to new folder
-	file.Path = dsti
-
+	mtx.Lock()
+	*subs = append(*subs, sub)
+	mtx.Unlock()
 	end := time.Since(start).Seconds()
-	fmt.Fprintf(os.Stdout, "File copied from %s to %s in %.2fs.\n", file.Path, dsti, end)
+	fmt.Fprintf(os.Stdout, "copyFile: file %s copied in %.2fs.\n", file.Name, end)
 	return
 }
 
@@ -163,13 +177,100 @@ func deleteall(dst string, ext string) {
 	if err := filepath.Walk(dst, func(path string, info os.FileInfo, err error) error {
 		if filepath.Ext(path) == ext {
 			if err := os.Remove(path); err != nil {
-				fmt.Fprintf(os.Stdout, "While deleting an error occurs: %v\n", err)
+				fmt.Fprintf(os.Stdout, "deleteAllFiles: could not detele file: %s, Error: %v\n", path, err)
+				log := Log{
+					Func: "deleteAll",
+					Err:  err,
+				}
+				logs = append(logs, log)
+				errFound = true
 			}
 		}
-		fmt.Fprintf(os.Stdout, "File %s has been deleted.\n", info.Name())
+		fmt.Fprintf(os.Stdout, "deleteAllFiles: file deleted without any errors: %s\n", info.Name())
 		return nil
 	}); err != nil {
-		fmt.Fprintf(os.Stderr, "while deleting %v\n", err)
+		fmt.Fprintf(os.Stderr, "deleteAllFiles: unable to delete any file. %v\n", err)
 		os.Exit(3)
 	}
+}
+
+func moveall(dst string, subs []Sub) {
+	// Create or check if the output dst exists
+	if _, err := os.Stat(dst); os.IsNotExist(err) {
+		err := os.Mkdir(dst, 0777)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "moveFile: could not create directory: %s, Error: %v\n", dst, err)
+			os.Exit(3)
+		}
+		fmt.Fprintf(os.Stdout, "moveFile: Output directory created: %s\n", dst)
+	}
+
+	// Delete and move files
+	for _, sub := range subs {
+		wg.Add(2)
+		go delete(sub.Path)
+		go create(sub, dst)
+	}
+	wg.Wait()
+}
+
+func delete(path string) {
+	defer wg.Done()
+	if err := os.Remove(path); err != nil {
+		fmt.Fprintf(os.Stdout, "deteleFile: could not delete file: %s, Error: %v\n", path, err)
+		errFound = true
+		log := Log{
+			Func: "delete",
+			Err:  err,
+		}
+		logs = append(logs, log)
+		return
+	}
+}
+
+func create(sub Sub, path string) {
+	defer wg.Done()
+	file := filepath.Join(path, "/", sub.Name)
+	f, err := os.OpenFile(file, syscall.O_RDWR|syscall.O_CREAT, 0777)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "createFile: could not create file: %s, Error: %v\n", file, err)
+		errFound = true
+		log := Log{
+			Func: "create",
+			Err:  err,
+		}
+		logs = append(logs, log)
+		return
+	}
+	defer f.Close()
+
+	if _, err := f.Write(sub.Body.Bytes()); err != nil {
+		fmt.Fprintf(os.Stdout, "createFile: could not save file: %s, Error: %v\n", file, err)
+		errFound = true
+		log := Log{
+			Func: "create",
+			Err:  err,
+		}
+		logs = append(logs, log)
+		return
+	}
+
+	fmt.Fprintf(os.Stdout, "createFile: file created without any error: %s\n", file)
+}
+
+func core(subs []Sub) {
+	// Check if flag MOVE is SET
+	if flags[move] != "" {
+		moveall(flags[move], subs)
+		return
+	}
+
+	// Default routine
+	// Delete all files inside default directory
+	deleteall(flags[path], flags[ext])
+	for _, sub := range subs {
+		wg.Add(1)
+		go create(sub, flags[path])
+	}
+	wg.Wait()
 }
