c35c4d982d1512a08b6c73c637ee0fea9827d286
sampalm <samuelpalmeira@outlook.com>
2018-05-19 23:16:08 -0300

    add new flag to download subtitles

--- core.go
+++ core.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/sampalm/subtitleManager/osb"
 )
 
 type Flag struct {
@@ -19,6 +22,7 @@ const (
 	ext
 	version
 	move
+	req
 )
 
 // options
@@ -30,6 +34,10 @@ const (
 )
 
 func init() {
+	// OSB Package
+	r := flag.String("r", "", "Download subtitles.")
+
+	// Manager
 	p := flag.String("p", "", "Set the root path.")
 	e := flag.String("e", ".srt", "Set the extension of the file.")
 	v := flag.String("v", "", "Set the version of the subtitle.")
@@ -41,7 +49,7 @@ func init() {
 	org := flag.Bool("o", false, "Organize all files in selected directy.")
 
 	flag.Parse()
-	fg.Get = []string{*p, *e, *v, *m}
+	fg.Get = []string{*p, *e, *v, *m, *r}
 	fg.Options = []bool{*d, *h, *o, *org}
 }
 
@@ -53,6 +61,58 @@ func main() {
 		break
 	case fg.Get[path] == "":
 		log.Println("Path must be defined.")
+		break
+	case fg.Get[req] != "":
+		var cErr = make(chan error)
+		var cHash = make(chan []string)
+		var cSub = make(chan []osb.Subtitle)
+		files, err := fg.FetchAll()
+		CheckErr("Request", 1, err)
+		for _, file := range files {
+			go func(file *os.File) {
+				hash, size, err := osb.HashFile(file)
+				if err != nil {
+					cErr <- err
+				}
+				cHash <- []string{fmt.Sprintf("%x", hash), fmt.Sprint(size)}
+			}(file)
+		}
+		for range files {
+			select {
+			case hashSize := <-cHash:
+				cDone := make(chan string)
+				subs, err := osb.SearchHashSub(hashSize[0], hashSize[1])
+				if err != nil {
+					fmt.Fprintf(os.Stdout, "Request: searchHashSub: %s", err.Error())
+				}
+				for _, sub := range subs {
+					go func(sub osb.Subtitle) {
+						//fmt.Fprintf(os.Stdout, "SUB FOUND: %s - Language: %s\n", sub.FileName, sub.LanguageName)
+						err := osb.DownloadSub(&sub)
+						if err != nil {
+							cErr <- err
+						}
+						// CREATE SUB
+						create(sub.FileName, sub.Body.Bytes(), fg.Get[path])
+						cDone <- sub.FileName
+					}(sub)
+				}
+				for range subs {
+					select {
+					case err := <-cErr:
+						fmt.Fprintf(os.Stdout, "Request: %s\n", err.Error())
+					case file := <-cDone:
+						fmt.Fprintf(os.Stdout, "Request: File %s downloaded with success!\n", file)
+					}
+				}
+			case err := <-cErr:
+				fmt.Fprintf(os.Stdout, "Request: hashFile: %s\n", err.Error())
+			case subs := <-cSub:
+				for _, sub := range subs {
+					fmt.Fprintf(os.Stdout, "Sub name: %s\nSub link: %s\n", sub.FileName, sub.DownloadLink)
+				}
+			}
+		}
 		break
 	case fg.Options[org]:
 		if !ConfirmAction("This task will move all files that will be found") {
@@ -104,11 +164,14 @@ func main() {
 		// delete files
 		fg.Deleteall(subs)
 		// copy files
+		cDone := make(chan bool)
 		for _, sub := range subs {
-			wg.Add(1)
-			go create(sub, fg.Get[path])
+			go create(sub.Name, sub.Body.Bytes(), fg.Get[path])
+			cDone <- true
 		}
-		wg.Wait()
+		for range subs {
+			<-cDone
+		}
 		break
 	default:
 		log.Println("Must define a flag before use, for help use -h.")
