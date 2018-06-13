53b90ea5f3c5a4fcefc50df982605db566cbdd5c
sampalm <samuelpalmeira@outlook.com>
2018-06-12 23:26:05 -0300

    add download subcommands

--- commands.go
+++ commands.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 var in io.Reader = os.Stdin
 var c = Controller{}
+
+func SliceString(value string) []string {
+	return strings.Split(strings.TrimSpace(value), ",")
+}
 
 func main() {
 	// Set subcommands
@@ -17,7 +22,7 @@ func main() {
 	categorizeCommand := flag.NewFlagSet("categorize", flag.ExitOnError)
 
 	//queryCommand := flag.NewFlagSet("query", flag.ExitOnError)
-	//downloadCommand := flag.NewFlagSet("download", flag.ExitOnError)
+	downloadCommand := flag.NewFlagSet("download", flag.ExitOnError)
 
 	// Copy subcommand flags
 	copySrc := copyCommand.String("src", "", "Defines source folder to copy.")
@@ -33,7 +38,10 @@ func main() {
 	// Query subcommand flags
 
 	// Download subcommand flags
-
+	downloadPath := downloadCommand.String("path", "", "Defines root file's folder.")
+	downloadLang := downloadCommand.String("lang", "eng", "Defines default language to download subtitles.")
+	downloadMLang := downloadCommand.String("multi", "", "Defines multiples languages. (Sep by comma)")
+	downloadScore := downloadCommand.Int("score", 0, "Defines rating score for subtitles.")
 	// Check that subcommand has been provided
 	if len(os.Args) < 2 {
 		fmt.Println("You must provide a command task.")
@@ -51,6 +59,9 @@ func main() {
 	case "categorize":
 		fmt.Println("Categorize task choosed.")
 		categorizeCommand.Parse(os.Args[2:])
+	case "download":
+		fmt.Println("Download task choosed.")
+		downloadCommand.Parse(os.Args[2:])
 	default:
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -89,5 +100,20 @@ func main() {
 			os.Exit(1)
 		}
 		Categorize(*categorizeDst, *categorizeSrc, PullCategorized)
+	}
+
+	// Handle DownloadCommand flags
+	if downloadCommand.Parsed() {
+		if *downloadPath == "" {
+			downloadCommand.PrintDefaults()
+			os.Exit(1)
+		}
+		c := Controller{
+			RootFolder:      *downloadPath,
+			DefaultLanguage: *downloadLang,
+			MultiLanguage:   SliceString(*downloadMLang),
+			RatingScore:     *downloadScore,
+		}
+		GetHashFiles(&c, *downloadPath, PullDir)
 	}
 }
